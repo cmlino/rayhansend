a48be991537e4212d6bd0418407628c45d2a2073
Christina Lino <cmlino@protonmail.com>
2020-09-13 22:23:38 -0400

    change

--- bot.py
+++ bot.py
@@ -1,7 +1,7 @@
 # bot.py
 import os
-
 import discord
+import csv
 TOKEN = 'NzU0ODQ1NzI0MDU4ODQ1Mjk0.X16q3A.zN9RjxnkkPKpSNU0UjMFVvofr9U'
 GUILD = 'CRIMB'
 
@@ -32,10 +32,26 @@ async def on_ready():
     members = '\n - '.join([member.name for member in guild.members])
     print(f'Guild Members:\n - {members}')
 
+def import_csv(file_name):
+    data = []
+    row_index = 0
+    with open(file_name, "r", encoding="utf-8", errors="ignore") as scraped:
+        reader = csv.reader(scraped, delimiter=',')
+        for row in reader:
+            if row:  # avoid blank lines
+                row_index += 1
+                columns = [str(row_index), row[0], row[1] ]
+                data.append(columns)
+    return data
+
+data = import_csv("edge_data.csv")
+last_row = data[-1]
+
 @client.event
 async def on_message(message):
     if '!ppl' in message.content.lower():
-        await message.channel.send('69')
-
+        await message.channel.send(
+            last_row
+        )
 
 client.run(TOKEN)

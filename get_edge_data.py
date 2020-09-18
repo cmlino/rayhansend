acaa16237cd936c209f33ce6d7907143882bcd59
Rylan <rylanoc@gmail.com>
2020-09-18 18:14:09 -0400

    collect last update time

--- get_edge_data.py
+++ get_edge_data.py
@@ -1,18 +1,19 @@
 import requests
 import re
 import csv
-import datetime 
+import datetime
+
 url = "https://portal.rockgympro.com/portal/public/5b68a6f4de953dcb1285dc466295eb59/occupancy"
 
 response = requests.request("GET", url)
 
 # print(response.text.encode('utf8'))
 # extract count using regex
-people = str((re.search(r"'' : (\d+)", response.text).group(1)))
-last_update = str((re.search(r"'' : [a-zA-Z0-9_]*)", response.text).group(1)))
+people = str((re.search(r"'count' : (\d+)", response.text).group(1)))
+last_update = str((re.search(r"\d+:\d+ [AP]M", response.text).group()))
 date_time = str((datetime.datetime.now()))
 
-fields = [people, date_time]
-with open('edge_data.csv', 'a') as csvfile:
-    writer = csv.writer(csvfile) 
+fields = [people, date_time, last_update]
+with open("edge_data.csv", "a") as csvfile:
+    writer = csv.writer(csvfile)
     writer.writerow(fields)

cc40e3bdc6268416f7bf0f7f66726d3fb86d4eb5
Christina Lino <cmlino@protonmail.com>
2020-10-29 19:54:22 -0400

    scrape each time

--- bot.py
+++ bot.py
@@ -1,13 +1,14 @@
 # bot.py
 import os
-import discord
+import re
 import csv
 import random
+import discord
+import requests
 from dotenv import load_dotenv
 
 load_dotenv()
 client = discord.Client()
-
 
 @client.event
 async def on_ready():
@@ -25,36 +26,41 @@ def fetch_most_recent(file_name):
 
 
 @client.event
+
+
 async def on_message(message):
+
     if "!ppl" in message.content.lower():
-        total_people, recent_time, updated_time = fetch_most_recent("edge_data.csv")
-        total_people = int(total_people)
-        print(total_people)
-        print(updated_time)
-        prime_time_string = '**:rotating_light: PRIME CLIMB TIME :rotating_light:** \n '
+        
+        response = requests.get("https://portal.rockgympro.com/portal/public/5b68a6f4de953dcb1285dc466295eb59/occupancy")
+
+        people = int((re.search(r"'count' : (\d+)", response.text).group(1)))
+        last_update = str((re.search(r"\d+:\d+ [AP]M", response.text).group()))
+       
+        prime_time_string = '**:rotating_light: PRIME CLIMB TIME :rotating_light:** \n'
         await message.channel.send(
-            f"{prime_time_string if total_people <= 15 else ''}{total_people} climbers (last updated at {updated_time})"
+            f"{prime_time_string if people <= 15 else ''}{people} climbers (last updated at {last_update})"
         )
 
     if "!holidays" in message.content.lower():
         await message.channel.send(
             """
-```
-The Edge is open:
-    New Years Eve and Day
-    MLK Day
-    President's Day
-    Columbus Day
-    Veteran's Day
+            ```
+            The Edge is open:
+                New Years Eve and Day
+                MLK Day
+                President's Day
+                Columbus Day
+                Veteran's Day
 
-The Edge is closed:
-    Easter Sunday
-    Mother's Day
-    Memorial Day Weekend (Sat-Mon)
-    July 4th
-    Labor Day
-    Thanksgiving
-```
+            The Edge is closed:
+                Easter Sunday
+                Mother's Day
+                Memorial Day Weekend (Sat-Mon)
+                July 4th
+                Labor Day
+                Thanksgiving
+            ```
             """
         )
 

0c0f144761fb9938793b553c636aeee8cac73186
Rylan <rylanoc@gmail.com>
2020-10-30 18:19:32 -0400

    remove dependancy on library that doesn't have all the holidays

--- bot.py
+++ bot.py
@@ -3,7 +3,7 @@ import os
 from discord.ext import commands
 
 from datetime import date
-import holidays
+import calendar, dateutil
 
 import csv
 import random
@@ -51,12 +51,54 @@ async def beta(ctx):
         random.choice(phrases['beta'])
     )
 
+def get_irregular_date(weekday: str, num: int, month: int, year: int) -> date:
+    '''
+    generates date object for an irregular date (eg. the 4th Sunday of April)
+
+    :param weekday: the day of the week (eg. "Sunday")
+    :param num: the week number (eg. 4)
+    :param month: the month number (eg. 4)
+    :param year: the year (eg. 2020)
+    :return: a datetime.date object representing the specified irregular date
+    '''
+    days = {
+        'Sunday': 0,
+        'Monday': 1,
+        'Tuesday': 2,
+        'Wednesday': 3,
+        'Thursday': 4,
+        'Friday': 5,
+        'Saturday': 6
+    }
+    day_index = days[weekday]
+
+    cal = calendar.Calendar(firstweekday=calendar.SUNDAY)
+    month = cal.monthdatescalendar(year, month)
+    irregular_date = month[num - 1, day_index]
+
+    # calendar pads the start of month with days from the previous month
+    # ensure we return a date from the proper month
+    if irregular_date.month != month:
+        irregular_date = month[num, day_index]
+
+    return irregular_date
 
 @bot.command()
-async def holidays(ctx):
-    closed = ['Easter', 'Mother\'s Day', 'Memorial Day Weekend', 'July 4th', 'Labor Day', 'Thanksgiving']
-    today = date.today()
-    us_holidays = holidays.US(years=today.year).items()
+async def holiday(ctx):
+    curr_day = date.today()
+    year = curr_day.year
+
+    closed = {
+            dateutil.easter.easter(year): 'Easter',
+            get_irregular_date('Sunday', 2, 5, year): 'Mother\'s Day',
+            get_irregular_date('Saturday', 4, 5, year): 'Memorial Day Weekend',
+            get_irregular_date('Sunday', 4, 5, year): 'Memorial Day Weekend',
+            get_irregular_date('Monday', 4, 5, year): 'Memorial Day',
+            date(year, 7, 4): 'July 4th',
+            get_irregular_date('Monday', 1, 9, year): 'Labor Day',
+            get_irregular_date('Thursday', 4, 11, year): 'Thanksgiving'
+    }
+
     if today in us_holidays:
         holiday_name = us_holidays[today]
         if holiday_name in closed:
@@ -64,6 +106,6 @@ async def holidays(ctx):
         else:
             await ctx.send('The Edge is **open** for {}'.format(holiday_name))
     else:
-        await ctx.send('No it\'s not :sad:')
+        await ctx.send('No it\'s not :upside_down:')
 
 bot.run(os.getenv("TOKEN"))

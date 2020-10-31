ada8dc453cfd6975ce67c09b0cc497fba8919299
Rylan <RylanOC@users.noreply.github.com>
2020-10-31 12:58:34 -0400

    Argument parser (#4)
    * move beta commands to json file for expandabilty
    
    * use discord's argument parsing library for cleaner and more efficient code
    
    * update to run bot instead of client

--- bot.py
+++ bot.py
@@ -1,24 +1,28 @@
 # bot.py
 import os
-import re
+import discord
+from discord.ext import commands
 import csv
 import random
 import json
-import discord
+import re
 import requests
 from dotenv import load_dotenv
 
 load_dotenv()
-client = discord.Client()
+bot = commands.Bot(command_prefix='!')
 
 def get_climbers():
     response = requests.get("https://portal.rockgympro.com/portal/public/5b68a6f4de953dcb1285dc466295eb59/occupancy")
 
+@bot.event
+async def on_ready():
+    print(f"{bot.user} has connected to Discord!")
     people = int((re.search(r"'count' : (\d+)", response.text).group(1)))
     last_update = str((re.search(r"\d+:\d+ [AP]M", response.text).group()))
 
     return (people, last_update)
-
+  
 def fetch_most_recent(file_name):
     data = ""
     with open(file_name, "r", encoding="utf-8", errors="ignore") as scraped:
@@ -35,18 +39,40 @@ async def on_ready():
     await client.change_presence(activity=discord.Activity(type=discord.ActivityType.watching, name=f"{data[0]} climbers"))
     print(f"{client.user} has connected to Discord!")
 
-@client.event
-async def on_message(message):
 
-    if "!ppl" in message.content.lower():
-        data = get_climbers()
-        prime_time_string = '**:rotating_light: PRIME CLIMB TIME :rotating_light:** \n'
-        await message.channel.send(
-            f"{prime_time_string if data[0] <= 15 else ''}{data[0]} climbers (last updated at {data[1]})"
-        )
+@bot.command()
+async def ppl(ctx):
+    total_people, recent_time, updated_time = fetch_most_recent("edge_data.csv")
+    total_people = int(total_people)
+    print(total_people)
+    print(updated_time)
+    prime_time_string = '**:rotating_light: PRIME CLIMB TIME :rotating_light:** \n '
+    await ctx.send(
+        f"{prime_time_string if total_people <= 15 else ''}{total_people} climbers (last updated at {updated_time})"
+    )
 
-    if "!holidays" in message.content.lower():
-        await message.channel.send(
+
+@bot.command()
+async def beta(ctx):
+    f = open('beta.json')
+    phrases = json.load(f)
+
+    await ctx.send(
+        random.choice(phrases['beta'])
+    )
+
+
+@bot.command()
+async def stretch(ctx):
+    stretches = [ "Russian twists", " https://www.youtube.com/watch?v=6A2V9Bu80J4", "Side planks", ":penguin:s", "Plank", "Hollow body", "Superman", "Pull up ", "Wall sit", "Cobra", "Weird hand thing", "Bicycles", "Sit up ", "Crunches", "Plank (1 min)", "Plank (1 min)", "Plank (1 min)", "Plank"  ]
+
+    await ctx.send(
+        f"{random.choice(stretches)} {random.randint(0, 200)} reps"
+    )
+
+@bot.command()
+async def holidays(ctx):
+    await ctx.send(
             """
             ```
             The Edge is open:
@@ -67,21 +93,4 @@ async def on_message(message):
             """
         )
 
-    if "!beta" in message.content.lower():
-        f = open('beta.json')
-        commands = json.load(f)
-
-        await message.channel.send(
-            random.choice(commands['beta'])
-        )
-
-    if "!stretch" in message.content.lower():
-        stretches = [ "Russian twists", " https://www.youtube.com/watch?v=6A2V9Bu80J4", "Side planks", ":penguin:s", "Plank", "Hollow body", "Superman", "Pull up ", "Wall sit", "Cobra", "Weird hand thing", "Bicycles", "Sit up ", "Crunches", "Plank (1 min)", "Plank (1 min)", "Plank (1 min)", "Plank"  ]
-
-        await message.channel.send(
-            f"{random.choice(stretches)} {random.randint(0, 200)} reps"
-        )
-
-
-
-client.run(os.getenv("TOKEN"))
+bot.run(os.getenv("TOKEN"))
